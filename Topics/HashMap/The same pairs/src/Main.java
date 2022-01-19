74e5f1e7866bc8e6e79c6efb16373999e2e208d7
alsignat <alsignat@gmail.com>
2022-01-19 22:00:10 +0300

    upd

---
+++ Topics/HashMap/The same pairs/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+class MapFunctions {
+
+    public static void calcTheSamePairs(Map<String, String> map1, Map<String, String> map2) {
+        map1.entrySet().retainAll(map2.entrySet());
+        System.out.println(map1.size());
+    }
+}
